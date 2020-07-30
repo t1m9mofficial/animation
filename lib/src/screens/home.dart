2ee0e2ac51eb51d31aa2871fc5bce98a42f6f182
Tahsin <t1m9m@Tahsins-MacBook-Pro.local>
2020-07-31 02:07:02 +0600

    first commit

---
+++ lib/src/screens/home.dart
@@ -0,0 +1,56 @@
+import 'package:flutter/material.dart';
+import '../widgets/cat.dart';
+
+class Home extends StatefulWidget {
+  HomeState createState() => HomeState();
+}
+
+class HomeState extends State<Home> with TickerProviderStateMixin {
+  Animation<double> catAnimation;
+  AnimationController catController;
+
+  initState() {
+    super.initState();
+
+    catController = AnimationController(
+      duration: Duration(seconds: 2),
+      vsync: this,
+    );
+
+    catAnimation = Tween(begin: 0.0, end: 100.0).animate(
+      CurvedAnimation(
+        parent: catController,
+        curve: Curves.easeIn
+      ),
+    );
+  }
+
+  onTap() {
+    catController.forward();
+  }
+
+  Widget build(context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Animation!'),
+      ),
+      body: GestureDetector(
+        child: buildAnimation(),
+        onTap: onTap,
+      ),
+    );
+  }
+
+  Widget buildAnimation() {
+    return AnimatedBuilder(
+      animation: catAnimation,
+      builder: (context, child) {
+        return Container(
+          child: child,
+          margin: EdgeInsets.only(top: catAnimation.value),
+        );
+      },
+      child: Cat(),
+    );
+  }
+}
